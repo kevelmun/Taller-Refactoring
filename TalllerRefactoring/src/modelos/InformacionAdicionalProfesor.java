b414162ee31ad9682e2f3b68bf2486dc726ba8c0
lnsalaza <lnsalaza@espol.edu.ec>
2021-08-12 17:29:56 -0500

    V1.2

--- TalllerRefactoring/src/modelos/InformacionAdicionalProfesor.java
+++ TalllerRefactoring/src/modelos/InformacionAdicionalProfesor.java
@@ -5,4 +5,32 @@ public class InformacionAdicionalProfesor {
     private String facultad;
     private double BonoFijo;
     
+    public double calcularSueldo(Profesor prof){
+        return prof.getInfo().getAñosdeTrabajo()*600 + prof.getInfo().getBonoFijo();
+    } 
+
+    public int getAñosdeTrabajo() {
+        return añosdeTrabajo;
+    }
+
+    public void setAñosdeTrabajo(int añosdeTrabajo) {
+        this.añosdeTrabajo = añosdeTrabajo;
+    }
+
+    public String getFacultad() {
+        return facultad;
+    }
+
+    public void setFacultad(String facultad) {
+        this.facultad = facultad;
+    }
+
+    public double getBonoFijo() {
+        return BonoFijo;
+    }
+
+    public void setBonoFijo(double BonoFijo) {
+        this.BonoFijo = BonoFijo;
+    }
+    
 }
